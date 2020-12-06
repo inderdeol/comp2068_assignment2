9164a004a563a6b5b10c1957197fc844555b60c6
inderdeol <200404339@student.georgianc.on.ca>
2020-12-06 15:05:02 -0500

    update functionality added to the app

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/insert', function(req, res){
   res.render('insert');
 })
 
-// save item to the database
+// save ad to the database
 router.post('/insert', function(req, res){
   var ads = new adsModel(req.body);
   // grab parameters from the form and save to the database. 
@@ -27,6 +27,25 @@ router.post('/insert', function(req, res){
     .catch(err => {
       res.status(400).send(err);
     });
+});
+
+/* GET update page from server */
+router.get('/update/:id', function (req, res) {
+  adsModel.findById(req.params.id, function (err, ad) {
+      if (err){
+        console.log(err);
+      } 
+      res.render('update', { ad: ad})
+  })
+});
+
+// post method to update send new data to mongoDb and update them.
+router.post('/update', function (req, res) {
+  console.log(req.body);
+  adsModel.findByIdAndUpdate(req.body.id, { item: req.body.item, description: req.body.description, price: req.body.price, condition: req.body.condition}, function (err, model) {
+      console.log(err);
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
